a0925dd3df20cd7605036a31f1b5ddb9e096b3b3
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2021-04-01 15:51:18 +0700

    change to view item

--- OnlineMart/src/components/ItemVIew/ItemView.component.jsx
+++ OnlineMart/src/components/ItemVIew/ItemView.component.jsx
@@ -26,6 +26,8 @@ export class ItemView extends Component {
         <h2>
           ${unit_price} for {unit_quantity}
         </h2>
+        {in_stock <= 0 ? <p> Out of stock </p> : <p>In Stock</p>}
+        <button disabled={in_stock <= 0 ? true : false}>Add to Cart</button>
       </div>
     ) : (
       <div>No Item selected</div>
