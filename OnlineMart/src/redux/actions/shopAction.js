9d85a303b6713ea571ce893a676a8cd8f741b33d
Mike0298 <takashishakai2012@gmail.com>
2021-04-09 20:20:31 +0700

    revert api

--- OnlineMart/src/redux/actions/shopAction.js
+++ OnlineMart/src/redux/actions/shopAction.js
@@ -5,7 +5,7 @@ export const getShop = () => (dispatch) => {
   dispatch({ type: LOADING_SHOP });
   axios
     .get(
-      "https://cors-anywhere.herokuapp.com/http://uts-12441376-ipa1.us-east-1.elasticbeanstalk.com/api/shop/getShop.php"
+      "http://uts-12441376-ipa1.us-east-1.elasticbeanstalk.com/api/shop/getShop.php"
     )
     .then((res) => {
       let shop = mergeShop(res.data);
