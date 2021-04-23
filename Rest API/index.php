8563032e29e349c0871b335cbdcf908bb244c273
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2021-04-23 15:54:47 +0700

    update api landing page

--- Rest API/index.php
+++ Rest API/index.php
@@ -1,14 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>My first page</title>
-</head>
-<body>
-    <h1>Welcome to OnlineMart API</h1>
-    <h2>By 12441376 of UTS</h2>
-    <h3>Links</h3>
-    <a href="http://onlinemart-12441376.herokuapp.com/">Online Mart Website</a>
-    <a href="https://github.com/Mike0298/OnlineMart"> Project Files </a>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>OnlineMart home</title>
+    <link rel="stylesheet" href="./index.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="container-box">
+        <div class="title">Welcome to OnlineMart API</div>
+        <div class="sub-title">
+          University of Technology Sydney - Internet Programming 32516 - Autumn
+          2021
+        </div>
+        <div class="author">Written by 12441376</div>
+        <div class="desc">
+          This page is just the landing page for the API, please click the on
+          the button below to be directed to the content you need
+        </div>
+        <div class="link">
+          <a href="http://onlinemart-12441376.herokuapp.com/">
+            <div class="button">
+              <div class="onlinemart">
+                <img
+                  src="https://www.freeiconspng.com/uploads/shopping-cart-icon-5.png"
+                  alt="assignment"
+                />
+                <div class="button-content">Assessment 1 Link</div>
+              </div>
+            </div>
+          </a>
+          <a href="https://github.com/Mike0298/OnlineMart"
+            ><div class="button">
+              <div class="github">
+                <img
+                  src="https://image.flaticon.com/icons/png/512/25/25231.png"
+                  alt="github"
+                />
+                <div class="button-content">Assessment 1 Files</div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
